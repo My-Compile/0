671d14040b89d72e842afb3ce157581708d1bb4b
My-Compile <75769819+My-Compile@users.noreply.github.com>
2020-12-22 12:12:04 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -1,2 +1,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/My-Compile/packajes passwall
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/My-Compile/packajes' feeds.conf.default
+#git clone https://github.com/My-Compile/packajes passwall
