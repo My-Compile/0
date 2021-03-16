af90f9fc2d29a894ec1bbb512a626085aa9e6aab
My-Compile <75769819+My-Compile@users.noreply.github.com>
2021-03-16 18:46:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,4 +8,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/My-Compile/packages' feeds.conf.default
