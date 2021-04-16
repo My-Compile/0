39a6854b1dba329bed6ef6c5ce22774a4ff8a1b0
00575 <75769819+00575@users.noreply.github.com>
2021-04-16 09:09:17 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,6 @@
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 rm -rf feeds/packages/libs/libcap/ && svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap feeds/packages/libs/libcap
 rm -rf feeds/packages/lang/golang/ && svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
 sed -i '/enable-jsonc/i\\t--disable-cloud \\' feeds/packages/admin/netdata/Makefile
